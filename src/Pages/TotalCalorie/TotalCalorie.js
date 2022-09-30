41f3928f8bc13b9a45296a6b749d1b2ecc8b087f
7rahib <rahibrashid002@gmail.com>
2022-09-30 19:32:30 +0600

    added day picker

--- src/Pages/TotalCalorie/TotalCalorie.js
+++ src/Pages/TotalCalorie/TotalCalorie.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
 import CalorieCard from '../Home/CalorieCard';
+import 'react-day-picker/dist/style.css';
 
 const TotalCalorie = () => {
+    const [selected, setSelected] = useState(new Date())
+
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+    }
     return (
         <div>
             <h3 className='text-center'>All Calorie Intakes</h3>
+            <div className='d-flex justify-content-center'>
+                <DayPicker
+                    mode="single"
+                    selected={selected}
+                    onSelect={setSelected}
+                    footer={footer}
+                />
+            </div>
             <div className='m-3'>
                 <div className='row'>
                     <div className='col col-12 col-md-12 col-lg-6 my-3'>
