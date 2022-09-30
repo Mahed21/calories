b01381b098ba3a2d4933f1b10df7f5260c3347db
7rahib <rahibrashid002@gmail.com>
2022-09-30 19:24:31 +0600

    navbar color changed

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-danger">
+        <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <Link type="button" class="btn btn-danger navbar-brand text-light" to="/">Calorie Calculator</Link>
+                <Link type="button" class="btn btn-light navbar-brand" to="/">Calorie Calculator</Link>
                 <div>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <div className='d-flex'>
                             <li class="nav-item">
-                                <Link to='/' type="button" class="btn btn-danger me-1">Home</Link>
+                                <Link to='/' type="button" class="btn btn-light me-1">Home</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to='/about' type="button" class="btn btn-danger me-1">About</Link>
+                                <Link to='/about' type="button" class="btn btn-light me-1">About</Link>
                             </li>
                             <li class="nav-item">
-                                <button type="button" class="btn btn-light">Login</button>
+                                <button type="button" class="btn btn-danger">Login</button>
                             </li>
                         </div>
                     </ul>
