042e8f7a69f29743d06d107471f7f6c27c9d2406
7rahib <rahibrashid002@gmail.com>
2022-09-30 11:18:37 +0600

    aa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
-=======
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Pages/Authentication/Login";
 import Register from "./Pages/Authentication/Register";
-import Home from "./Pages/Home/Home";
->>>>>>> 460b75595bd50824dd5eb1660539f330ba7d42f7
 
 function App() {
   return (
