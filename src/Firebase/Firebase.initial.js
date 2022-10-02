52c290a97d2255b4883ab2a7a3d53e32d9c8ca57
7rahib <rahibrashid002@gmail.com>
2022-10-02 12:16:20 +0600

    firebase env added

--- src/Firebase/Firebase.initial.js
+++ src/Firebase/Firebase.initial.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 const initializeAuthentication = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyCO7W8X-sHYXgUD8pJDq944r-GTVK0Hm64",
-    authDomain: "medicare-21a29.firebaseapp.com",
-    projectId: "medicare-21a29",
-    storageBucket: "medicare-21a29.appspot.com",
-    messagingSenderId: "777739868936",
-    appId: "1:777739868936:web:792cd498a1878558adeabd",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
   };
   const app = initializeApp(firebaseConfig);
 };
