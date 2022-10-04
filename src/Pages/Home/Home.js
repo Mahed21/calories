4584cbdddcaf029f9bc592b6f0779be002d84a31
7rahib <rahibrashid002@gmail.com>
2022-10-04 23:51:38 +0600

    firebase removed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,12 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import AddCalorie from "./AddCalorie";
 
 const Home = () => {
-  const { user } = useAuth0();
   return (
     <Container fluid>
       <Row>
         <Col xs={12} lg={12}>
-          <h5 className="text-center mt-5">Hello {user?.name}</h5>
           <AddCalorie />
         </Col>
       </Row>
--- src/auth0Provider.js
+++ src/auth0Provider.js
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
-    const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+    const domain = 'dev-y2zxsjoi.us.auth0.com';
+    const clientId = 'FbABiA8sm1SLbWnh3IM8Kn3TyBjbQYsL';
     const navigate = useNavigate();
 
     const onRedirectCallback = (appState) => {
